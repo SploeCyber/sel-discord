b0e1707e21c087cd3a0a113282cf6807a9e03853
SploeCyber <37950018+SploeCyber@users.noreply.github.com>
2020-11-25 06:23:05 +0700

    except Exception then pass

--- examples/proxied_token_generator.py
+++ examples/proxied_token_generator.py
@@ -4,7 +4,7 @@ import secrets
 import itertools
 
 workers = 10
-user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"
+user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.66 Safari/537.36"
 output_file = open("tokens.txt", "a")
 
 with open("proxies.txt", encoding="UTF-8", errors="ignore") as f:
@@ -22,9 +22,7 @@ class Worker(Thread):
     def do_task(self):
         proxy_url = "http://%s" % next(proxy_iter)
         with seldiscord.Session(user_agent, proxy_url) as dsc:
-            dsc.register(
-                username=generate_name()
-            )
+            dsc.register(username=generate_name())
             dsc.gateway()
             save_token(dsc.token)
         
@@ -32,8 +30,8 @@ class Worker(Thread):
         while 1:
             try:
                 self.do_task()
-            except Exception as err:
-                print("Worker error:", err, type(err))
+            except Exception:
+                pass
 
 for _ in range(workers):
     Worker().start()
